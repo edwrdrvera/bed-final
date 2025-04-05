8ba76a2671b91a71824aee61bd82b47241441ab7
edwrdrvera <edwardr1213@gmail.com>
2025-04-04 20:22:41 -0500

    Defined an interface for Location inputs.

--- src/api/v1/models/locationModel.ts
+++ src/api/v1/models/locationModel.ts
@@ -1,8 +1,14 @@
-import { Pokemon } from "./pokemonModel";
+import { PokemonData } from "./pokemonModel";
+
+export interface LocationInput {
+	addressName: string;
+	terrain: string;
+	pokemon: string[];
+}
 
 export interface Location {
 	id: string;
 	addressName: string;
 	terrain: string;
-	pokemon: Pokemon[];
+	pokemon: PokemonData[];
 }
