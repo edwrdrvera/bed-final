ee09e9b488c03889acef86f3f2eb66d1c9c66f38
edwrdrvera <edwardr1213@gmail.com>
2025-04-04 20:25:10 -0500

    Created controller function for creating a location.

---
+++ src/api/v1/controllers/locationController.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction } from "express";
+import * as locationService from "../services/locationService";
+import { Location } from "../models/locationModel";
+
+export const createLocation = async (
+	req: Request,
+	res: Response,
+	next: NextFunction
+): Promise<void> => {
+	try {
+		console.log(req.body);
+		const newLocation: Location = await locationService.createLocation(
+			req.body
+		);
+		res.status(201).json(newLocation);
+	} catch (error) {
+		console.error("Error in createLocation controller:", error);
+		res.status(500).json({
+			message: "Internal server error occurred while creating location.",
+		});
+	}
+};
