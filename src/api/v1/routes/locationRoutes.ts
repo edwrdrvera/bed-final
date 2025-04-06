d8ffba9ee2bdc12e55df0eba87ba18a6dacbfcfa
edwrdrvera <edwardr1213@gmail.com>
2025-04-05 20:43:37 -0500

    Implemented validation middleware to route handlers.

--- src/api/v1/routes/locationRoutes.ts
+++ src/api/v1/routes/locationRoutes.ts
@@ -1,15 +1,28 @@
 import express, { Router } from "express";
 import * as locationController from "../controllers/locationController";
+import { validateRequest } from "../middleware/validate";
+import {
+	locationSchema,
+	updateLocationSchema,
+} from "../validation/locationValidation";
 
 const router: Router = express.Router();
 
-router.post("/", locationController.createLocation);
+router.post(
+	"/",
+	validateRequest(locationSchema),
+	locationController.createLocation
+);
 
 router.get("/", locationController.getAllLocations);
 
 router.get("/:id", locationController.getLocationById);
 
-router.put("/:id", locationController.updateLocation);
+router.put(
+	"/:id",
+	validateRequest(updateLocationSchema),
+	locationController.updateLocation
+);
 
 router.delete("/:id", locationController.deleteLocation);
 
