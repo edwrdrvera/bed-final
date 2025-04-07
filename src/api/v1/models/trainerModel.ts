a014b73dc503f1312e9a521a2a4cf1ec0a6ff7c1
edwrdrvera <edwardr1213@gmail.com>
2025-04-06 21:35:00 -0500

    Added region to trainer model.

--- src/api/v1/models/trainerModel.ts
+++ src/api/v1/models/trainerModel.ts
@@ -4,17 +4,20 @@ export interface Trainer {
 	id: string;
 	name: string;
 	age: number;
+	region: string;
 	team: PokemonInTeam[];
 }
 
 export interface TrainerInput {
 	name: string;
 	age: number;
+	region: string;
 	team: string[];
 }
 
 export interface TrainerUpdate {
 	name?: string;
 	age?: number;
+	region?: string;
 	team?: string[];
 }
