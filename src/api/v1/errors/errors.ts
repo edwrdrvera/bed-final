c7006291bda56b03f15eda24c9e7e87a4da567c0
edwrdrvera <edwardr1213@gmail.com>
2025-04-16 21:37:11 -0500

    Created more error classes.

--- src/api/v1/errors/errors.ts
+++ src/api/v1/errors/errors.ts
@@ -1,13 +1,33 @@
 import { HTTP_STATUS } from "../../../constants/httpConstants";
 
 /**
+ * Base error class for application errors.
+ * Extends the built-in Error class to include an error code and status code.
+ */
+export class AppError extends Error {
+	/**
+	 * Creates a new AppError instance.
+	 * @param {string} message - The error message.
+	 * @param {string} code - The error code.
+	 * @param {number} statusCode - The http response code.
+	 */
+	constructor(
+		public message: string,
+		public code: string,
+		public statusCode: number
+	) {
+		super(message);
+		this.name = this.constructor.name;
+		Object.setPrototypeOf(this, new.target.prototype);
+		Error.captureStackTrace(this, this.constructor);
+	}
+}
+
+/**
  * Class representing a repository error.
  * Extends the built-in Error class to include an error code.
  */
-export class RepositoryError extends Error {
-	code: string;
-	statusCode: number;
-
+export class RepositoryError extends AppError {
 	/**
 	 * Creates a new RepositoryError instance.
 	 * @param {string} message - The error message.
@@ -19,11 +39,7 @@ export class RepositoryError extends Error {
 		code: string,
 		statusCode: number = HTTP_STATUS.INTERNAL_SERVER_ERROR
 	) {
-		super(message);
-		this.name = "RepositoryError";
-		this.code = code;
-		this.statusCode = statusCode;
-		Object.setPrototypeOf(this, RepositoryError.prototype);
+		super(message, code, statusCode);
 	}
 }
 
@@ -31,24 +47,58 @@ export class RepositoryError extends Error {
  * Class representing a service error.
  * Extends the built-in Error class to include an error code.
  */
-export class ServiceError extends Error {
-	code: string;
-	statusCode: number;
-
+export class ServiceError extends AppError {
 	/**
 	 * Creates a new ServiceError instance.
 	 * @param {string} message - The error message.
 	 * @param {string} code - The error code.
-	 * @param {number} code - The the http response code.
+	 * @param {number} statusCode - The http response code.
 	 */
 	constructor(
 		message: string,
-		code: string,
+		code: string = "SERVICE_ERROR",
 		statusCode: number = HTTP_STATUS.INTERNAL_SERVER_ERROR
 	) {
-		super(message);
-		this.name = "ServiceError";
-		this.code = code;
-		this.statusCode = statusCode;
+		super(message, code, statusCode);
+	}
+}
+
+/**
+ * Class representing a authentication error.
+ * Extends the built-in Error class to include an error code.
+ */
+export class AuthenticationError extends AppError {
+	/**
+	 * Creates a new AuthenticationError instance.
+	 * @param {string} message - The error message.
+	 * @param {string} code - The error code.
+	 * @param {number} statusCode - The http response code.
+	 */
+	constructor(
+		message: string,
+		code: string = "AUTHENTICATION_ERROR",
+		statusCode: number = HTTP_STATUS.UNAUTHORIZED
+	) {
+		super(message, code, statusCode);
+	}
+}
+
+/**
+ * Class representing a authorization error.
+ * Extends the built-in Error class to include an error code.
+ */
+export class AuthorizationError extends AppError {
+	/**
+	 * Creates a new AuthorizationError instance.
+	 * @param {string} message - The error message.
+	 * @param {string} code - The error code.
+	 * @param {number} statusCode - The http response code.
+	 */
+	constructor(
+		message: string,
+		code: string = "AUTHORIZATION_ERROR",
+		statusCode: number = HTTP_STATUS.FORBIDDEN
+	) {
+		super(message, code, statusCode);
 	}
 }
