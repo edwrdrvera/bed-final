f4e7a3f12f439315ed9748ad49664f3b0bc78d9b
edwrdrvera <edwardr1213@gmail.com>
2025-04-19 14:01:02 -0500

    Refactor jest setup mock structure.

--- tests/unit/jest.setup.ts
+++ tests/unit/jest.setup.ts
@@ -1,7 +1,22 @@
+// Always mock firebase in every test
 jest.mock("../../config/firebaseConfig", () => ({
-	default: {
+	auth: {
+		verifyIdToken: jest.fn(),
+		getUser: jest.fn(),
+	},
+	db: {
 		collection: jest.fn(),
 		runTransaction: jest.fn(),
 		batch: jest.fn(),
 	},
 }));
+
+// Reset all mocks afte each test
+afterEach(() => {
+	jest.clearAllMocks();
+});
+
+// Cleanup after all tests in a file
+afterAll(() => {
+	jest.resetModules();
+});
