59ed11aa79ad3acef67f26ae5e63e591ac687de4
edwrdrvera <edwardr1213@gmail.com>
2025-04-18 19:00:39 -0500

    Added JSDoc for MiddlewareFunction, RequestBody, and RequestData types.

--- src/api/v1/types/expressTypes.ts
+++ src/api/v1/types/expressTypes.ts
@@ -1,13 +1,31 @@
 import { Request, Response, NextFunction } from "express";
 
+/**
+ * Represents the structure of a middleware function in an Express application.
+ *
+ * @param req - The request object
+ * @param res - The response object
+ * @param next - The next middleware function
+ */
 export type MiddlewareFunction = (
 	req: Request,
 	res: Response,
 	next: NextFunction
 ) => void;
 
+/**
+ * Generic type for the request body in Express.
+ */
 export type RequestBody = Record<string, unknown>;
 
+/**
+ * General type for the request data in Express.
+ *
+ * @template T - The type of the request body.
+ * @property body - The parsed request body according to T.
+ * @property params - Route parameters as key-value pairs.
+ * @property query - Query containing the parsed query string parameters.
+ */
 export type RequestData<T extends RequestBody = RequestBody> = {
 	body: T;
 	params: Record<string, string>;
